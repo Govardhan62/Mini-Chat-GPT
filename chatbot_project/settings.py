0c55efbee306a1073173c052587389c25e38578d
govardhan <115453040+Govardhan62@users.noreply.github.com>
2025-03-05 12:14:57 +0530

    changes done

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
     "default": {
